357eb8e56f6f1ee0bad7335bccf048fc3054c4b3
nick-ciereck <nickciereck@gmail.com>
2026-04-19 15:00:49 -0700

    Update page.js

--- app/library/page.js
+++ app/library/page.js
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 export default function PlantLibrary() {
   const [plants, setPlants] = useState([]);
